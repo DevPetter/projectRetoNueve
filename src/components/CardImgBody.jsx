54d865b59681d167879a244ed132225f1305f5c1
petter <petter0915@gmail.com>
2022-05-15 20:23:25 -0500

    add modified bodyCSS, CardImgBody and CardTXTBody, nuevas configuraciones front

--- src/components/CardImgBody.jsx
+++ src/components/CardImgBody.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import images from "../assets/img/images.js";
+
 import "../assets/style/body.css";
 
 export const CardImgBody = () => {
--- src/components/CardTextBody.jsx
+++ src/components/CardTextBody.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Card } from "react-bootstrap";
+
+import "../assets/style/body.css";
 
 export const CardTextBody = () => {
   return (
-    <div>CardTextBody</div>
-  )
-}
+    <>
+      <Card className="cardtxtBody">
+        <Card.Body>
+          <Card.Title>Lorem Ipsum</Card.Title>
+          <Card.Text>
+          es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
