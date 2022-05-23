fdef8c0e7cc8d6282197c608b7917ceb83fa0388
petter <petter0915@gmail.com>
2022-05-22 19:48:13 -0500

    Imagen de fondo en nuestro reto

--- src/assets/img/images.js
+++ src/assets/img/images.js
@@ -3,10 +3,12 @@ import logo from "../img/logo-fondo-transparente.png";
 import grupoUno from "../img/atrevete.jpg";
 import medellin from "../img/medellin.jpg";
 import integrantes from "../img/integrantes1.jpg"
-
+import paramoBelmira from "../img/paramoBelmira.jpg"
+ 
 export default {
   imgLogo: logo,
   imgGrupo: grupoUno,
   imgMedellin: medellin,
   imgIntegrantes: integrantes,
+  imgParamoBelmira:paramoBelmira
 };
--- src/page/OurChallenge.jsx
+++ src/page/OurChallenge.jsx
@@ -1,10 +1,14 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
+
+import images from "../assets/img/images";
 import "../assets/style/ourReto.css"
 
 export const OurChallenge = () => {
   return (
     <div className="our-reto-style">
       <h1>Nuestro reto</h1>
+      <img src={images.imgParamoBelmira} className="img-fondo"/>
     </div>
   );
 };
